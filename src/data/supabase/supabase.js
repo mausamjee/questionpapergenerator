d68989ff564d8a2361a32adecda68f658f500c32
pistleai <vickymausam01@gmail.com>
2025-12-31 12:26:24 +0530

    supabase v2

--- src/data/supabase/supabase.js
+++ src/data/supabase/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = import.meta.env.NEXT_PUBLIC_SUPABASE_KEY
+// Next.js mein hum 'process.env' use karte hain
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl || '', supabaseKey || '')
-
